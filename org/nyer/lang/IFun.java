590dd81647dd76e4c59928ceb8fe1fad62c34590
nyer <ls5231@gmail.com>
2013-11-21 20:33:35 +0800

    ci

--- org/nyer/lang/IFun.java
+++
@@ -1,7 +0,0 @@
-package org.nyer.lang;
-
-public interface IFun {
-	public Object invoke(Env env, Object[] arguments) throws Exception;
-	public String getName();
-	public String[] getParameters();
-}
--- org/nyer/lang/primitive/Void.java
+++
@@ -1,8 +0,0 @@
-package org.nyer.lang.primitive;
-
-public class Void {
-	@Override
-	public String toString() {
-		return "Void";
-	}
-}
