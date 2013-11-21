41c7075ff11503f91790dd21cbeba1823c553b1a
nyer <ls5231@gmail.com>
2013-11-21 20:30:27 +0800

    rename package name

--- org/nyer/pyjs/IFun.java
+++
@@ -1,7 +0,0 @@
-package org.nyer.pyjs;
-
-public interface IFun {
-	public Object invoke(Env env, Object[] arguments) throws Exception;
-	public String getName();
-	public String[] getParameters();
-}
--- org/nyer/pyjs/primitive/Void.java
+++
@@ -1,8 +0,0 @@
-package org.nyer.pyjs.primitive;
-
-public class Void {
-	@Override
-	public String toString() {
-		return "Void";
-	}
-}
