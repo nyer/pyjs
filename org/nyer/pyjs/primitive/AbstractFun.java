41ac130c55bfe9496a5cff7ee8bed1c641e69ca4
nyer <ls5231@gmail.com>
2013-11-27 13:48:40 +0800

    remove unused method

--- org/nyer/pyjs/primitive/AbstractFun.java
+++ org/nyer/pyjs/primitive/AbstractFun.java
@@ -10,9 +10,7 @@
 
 package org.nyer.pyjs.primitive;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.nyer.pyjs.Env;
 import org.nyer.pyjs.IFun;
@@ -34,11 +32,6 @@ public abstract class AbstractFun implements IFun {
 		return "function";
 	}
 	
-	
-	protected List<IFun> evalArguments(Env env, List<IFun> arguments) {
-		List<IFun> args = new ArrayList<IFun>(arguments.size());
-		return args;
-	}
 	@Override
 	public String toString() {
 		return "Function [parameters="
